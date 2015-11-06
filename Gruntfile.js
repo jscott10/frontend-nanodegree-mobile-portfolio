6d1e7be924e38e4473a99c22857f10a836502a0d
James Scott <jscott@jscott10.com>
2015-11-06 01:07:42 -0500

    fix: pagespeed issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,18 @@ grunt.initConfig({
 	pkg: grunt.file.readJSON('package.json'),
 
 	imagemin: {                          // Task
+		// static: {                          // Target
+		// 	options: {                       // Target options
+		// 		optimizationLevel: 3,
+		// 		svgoPlugins: [{ removeViewBox: false }],
+		// 		use: [mozjpeg()]
+		// 	},
+		// 	files: {                         // Dictionary of files
+		// 		'dist/img.png': 'src/img.png', // 'destination': 'source'
+		// 		'dist/img.jpg': 'src/img.jpg',
+		// 		'dist/img.gif': 'src/img.gif'
+		// 	}
+		// },
 		dynamic: {                         // Another target
 			files: [{
 				expand: true,                  // Enable dynamic expansion
@@ -47,9 +59,16 @@ grunt.initConfig({
 				collapseWhitespace: true
 			},
 			files: {                                   // Dictionary of files
-				'index.html': 'html-source/index.html'     // 'destination': 'source'
+				'index.html': 'html-source/index.html',     // 'destination': 'source'
+				// 'dist/contact.html': 'src/contact.html'
 			}
 		}
+		// dev: {                                       // Another target
+		// 	files: {
+		// 		'dist/index.html': 'src/index.html',
+		// 		'dist/contact.html': 'src/contact.html'
+		// 	}
+		// }
 	},
   	pagespeed: {
 		options: {
@@ -72,9 +91,18 @@ grunt.initConfig({
 				threshold: 50
 			}
 		}
+		// paths: {
+		// 	options: {
+		// 		paths: ["/frontend-nanodegree-mobile-portfolio"],
+		// 		locale: "en_GB",
+		// 		strategy: "desktop",
+		// 		threshold: 80
+		// 	}
+		// }
 	}
 });
 
+// Load the plugin that provides the "uglify" task.
 grunt.loadNpmTasks('grunt-contrib-imagemin');
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
